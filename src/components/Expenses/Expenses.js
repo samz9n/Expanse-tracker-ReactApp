c4c1b08ea43738804b6903c34ce258647faa58be
samz9n <samuel.langhoff@outlook.com>
2022-12-07 14:48:36 +0200

    added expanses listing and some conditinal rendering with filtering by year

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,15 +1,28 @@
-import React from 'react'
-import Card from '../UI/Card'
-import ExpenseItem from './ExpenseItem'
-import './Expenses.css'
+import React, { useState } from 'react';
+import Card from '../UI/Card';
+import ExpenseItem from './ExpenseItem';
+import './Expenses.css';
+import ExpensesFilter from './ExpensesFilter';
+import { ExpensesList } from './ExpensesList';
 
 export default function Expenses(props) {
-  return (
-    <Card className='expenses'>
-        <ExpenseItem title={props.expenses[0].title} amount={props.expenses[0].amount} date={props.expenses[0].date} />
-        <ExpenseItem title={props.expenses[1].title} amount={props.expenses[1].amount} date={props.expenses[1].date} />
-        <ExpenseItem title={props.expenses[2].title} amount={props.expenses[2].amount} date={props.expenses[2].date} />
-        <ExpenseItem title={props.expenses[3].title} amount={props.expenses[3].amount} date={props.expenses[3].date} />
-     </Card>
-  )
+	const [ filteredYear, setFilteredYear ] = useState('2020');
+
+	const filterChangeHandler = (selectedYear) => {
+		setFilteredYear(selectedYear);
+	};
+
+	const filteredExpenses = props.expenses.filter((expense) => {
+		return expense.date.getFullYear().toString() === filteredYear;
+	});
+
+
+	return (
+		<div>
+			<Card className="expenses">
+				<ExpensesFilter selected={filteredYear} onChangeFilter={filterChangeHandler} />
+				<ExpensesList items={filteredExpenses} />
+			</Card>
+		</div>
+	);
 }
--- src/components/NewExpense/NewExpense.js
+++ src/components/NewExpense/NewExpense.js
@@ -1,12 +1,31 @@
-import React from 'react'
-import './NewExpense.css'
-import { ExpenseForm } from './ExpenseForm'
+import React, { useState } from 'react';
+import './NewExpense.css';
+import { ExpenseForm } from './ExpenseForm';
 
-export const NewExpense = () => {
-  return (
-    <div className='new-expense'>
-        <ExpenseForm />
-    </div>
-  )
-}
+export const NewExpense = (props) => {
+	const saveExpenseDataHandler = (enteredExpenseData) => {
+		const expenseData = {
+			...enteredExpenseData,
+			id: Math.random().toString()
+		};
+		props.onAddExpense(expenseData);
+    setIsEditing(false);
+	};
 
+  const [isEditing, setIsEditing] = useState(false);
+
+  const startEditingHandler = () => {
+    setIsEditing(true);
+  }
+
+  const stopEditingHandler = () => {
+    setIsEditing(false);
+  }
+
+	return (
+		<div className="new-expense">
+			{!isEditing && <button onClick={startEditingHandler}>Add new Expense</button>}
+			{isEditing && <ExpenseForm onSaveExpenseData={saveExpenseDataHandler} onCancel={stopEditingHandler} />}
+		</div>
+	);
+};
